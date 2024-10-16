d3dfb3ebe2a6b735254114b412b5a511701cd6cb
mariibykova <101357988+mariibykova@users.noreply.github.com>
2024-10-17 00:37:18 +0400

    Решение для файла Tern2

--- Hw1_Conversion of conditions/Tern2.js
+++ Hw1_Conversion of conditions/Tern2.js
@@ -1,12 +1,84 @@
+//Конструкция if..else
 function manyChecks() {
   let a = Math.floor(Math.random() * 20) + 1;
+   a = 10;
   console.log(`a = ${a}`);
-  
-  return (
-    a > 10 ? 'a is bigger than 10' : 'a is less than or equal to 10 ' + (a === 5 ? 'an example of a special case' : '')) + (a === 15 ? 'but a is not 15' : '')+ (a > 5 ? 'and a is greater than 5' : 'and a is less than or equal to 5 ') + (a % 2 ? ' and a is odd' : ' and a is even ');
+  let s = '';
+  if (a > 10) {
+    s += 'a is bigger than 10';
+  } 
+  else {
+    s += 'a is less than or equal to 10 ';
+    if (a === 5) {
+      s += 'an example of a special case';
+    }
+  }
+  if (a === 15) {
+    s += 'but a is not 15';
+  }
+  if (a > 5) {
+    s += 'and a is greater than 5';
+  } 
+  else {
+    s += 'and a is less than or equal to 5 ';
+  }
+  if (a % 2) {
+    s += ' and a is odd';
+  } 
+  else {
+    s += ' and a is even';
+  }
+  return s;
 }
+console.log(manyChecks());
 
-manyChecks();
 
-// условие с условным (тернарным) оператором перевести в if...else И switch()
-// результат выводить в консоль, с пощью console.log()
+// Конструкция swich
+function manyChecks() {
+  let a = Math.floor(Math.random() * 20) + 1;
+  a = 10;
+  console.log(`a = ${a}`);
+  let s = '';
+  switch (true) {
+    case (a > 10):
+      s += 'a is bigger than 10';
+      break;
+    case (a <= 10):
+      s += 'a is less than or equal to 10 ';
+      if (a === 5) {
+        s += 'an example of a special case';
+      }
+      break;
+  }
+  switch (true) {
+    case (a === 15):
+      s += 'but a is not 15';
+      break;
+  }
+  switch (true) {
+    case (a > 5):
+      s += 'and a is greater than 5';
+      break;
+    default:
+      s += 'and a is less than or equal to 5 ';
+      break;
+  }
+  switch (true) {
+    case (a % 2):
+      s += ' and a is odd';
+      break;
+    default:
+      s += ' and a is even';
+      break;
+  }
+  return s;
+}
+console.log(manyChecks()); 
+
+
+
+
+
+
+
+
