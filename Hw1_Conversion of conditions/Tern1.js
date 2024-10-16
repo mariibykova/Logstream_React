5eab9e147f2fc8221f7a4138ff7438b0252d9578
mariibykova <101357988+mariibykova@users.noreply.github.com>
2024-10-17 00:25:49 +0400

    Решение для файла Tern1

--- Hw1_Conversion of conditions/Tern1.js
+++ Hw1_Conversion of conditions/Tern1.js
@@ -1,5 +1,59 @@
+//Конструкция с if...else
 let a = Math.floor(Math.random() * 100);
-(a > 10 ? a : a * 2) > 5 ? (2 * a) + 1 : (a < 3 ? 1 : 2 * (a - 2)) > 4 ? 5 : (a % 2 == 0 ? 6 : 7);
+if (a > 10) {
+    if ((a) > 5) {
+        a = (2 * a) + 1;
+    } 
+    else {
+        if (a < 3) {
+            a = 1;
+        } 
+        else {
+            if (2 * (a - 2) > 4) {
+                a = 5;
+            } 
+            else {
+                a = (a % 2 === 0) ? 6 : 7;
+            }
+        }
+    }
+} 
+else {
+    if ((a * 2) > 5) {
+        a = (2 * a) + 1;
+    } 
+    else {
+        if (a < 3) {
+            a = 1;
+        } 
+        else {
+            if (2 * (a - 2) > 4) {
+                a = 5;
+            } 
+            else {
+                a = (a % 2 === 0) ? 6 : 7;
+            }
+        }
+    }
+}
+console.log(a);
 
-// условие с условным (тернарным) оператором перевести в if...else И switch()
-// результат выводить в консоль, с пощью console.log()
+//Конструкция switch()
+let a = Math.floor(Math.random() * 100);
+switch (true) {
+    case (a > 10 && (a > 5)):
+        a = (2 * a) + 1;
+        break;
+    case (a <= 10 && (a * 2 > 5)):
+        a = (2 * a) + 1;
+        break;
+    case (a < 3):
+        a = 1;
+        break;
+    case (2 * (a - 2) > 4):
+        a = 5;
+        break;
+    default:
+        a = (a % 2 === 0) ? 6 : 7;
+}
+console.log(a);
